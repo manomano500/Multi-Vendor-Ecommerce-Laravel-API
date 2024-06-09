bfb09a44a525134ea12bda22ae11b6aeebdc719a
mahjouba <mano500mano13@gmail.com>
2024-06-09 04:14:23 +0200

    ,,
    Signed-off-by: mahjouba <mano500mano13@gmail.com>

---
+++ app/Http/Controllers/Auth/StoreController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\StoreResource;
+use App\Models\Store;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class StoreController extends Controller
+{
+    public function show(Request $request)
+    {
+        $store = $request->user()->store; // Assuming 'store' is a relationship method in the User model
+
+        if (!$store) {
+            return response()->json(['message' => 'No store found for this user'], 404);
+        }
+        return new StoreResource($store);
+
+    }
+
+
+    public function update(Request $request)
+    {
+        $store = Auth::user()->store;
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'string', 'max:255',
+            'description' => 'nullable', 'string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5048',
+            'status' => 'string|in:active,inactive'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        try {
+            $updatedFields = $request->only([
+                'name',
+                'description',
+                'status',
+
+            ]);
+
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $path = $image->store('images/stores', 'public');
+                $updatedFields['image'] = $path;
+            }
+
+
+            $store->update($updatedFields);
+            return response()->json(['message' => 'Store updated successfully', 'data' => new StoreResource($store)], 200);
+
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $validator->errors()], 500);
+        }
+
+        //
+    }
+
+
+    public function destroy(Store $store)
+    {
+        //
+    }
+
+
+    /**
+     * Customer can become a vendor
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     *
+     */
+    public function becomeVendor(Request $request)
+    {
+        $user = Auth::user();
+
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'category' => ['required', 'string', 'exists:categories,id'],
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5048',
+            'address' => 'nullable|string|max:255',
+            'city' => 'nullable|string|max:255',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        try {
+            DB::transaction(function () use ($request, $user) {
+
+                $new_store = new Store(
+                    [
+                        'user_id' => $user->id,
+                        'name' => $request->name,
+                        'description' => $request->description,
+                        'category_id' => $request->category,
+                        'city_id' => $request->city,
+                        'address' => $request->address,
+
+                    ]
+                );
+
+                if ($request->hasFile('image')) {
+                    $image = $request->file('image');
+                    $path = $image->store('images/stores', 'public');
+                    $new_store->image = $path;
+                }
+                $new_store->save();
+                $user->role_id = 2;
+                $user->save();
+
+            });
+            return response()->json(['message' => 'You are now a vendor'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $validator->errors()], 500);
+        }
+
+    }
+}
--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\StoreResource;
-use App\Models\Store;
-use Illuminate\Http\Request;
+use App\Models\City;
+use App\Http\Requests\StoreCityRequest;
+use App\Http\Requests\UpdateCityRequest;
 
-class StoreController extends Controller
+class CityController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index()
     {
-
-        $store = $request->user()->store; // Assuming 'store' is a relationship method in the User model
-
-        if (!$store) {
-            return response()->json(['message' => 'No store found for this user'], 404);
-        }
-        return new StoreResource($store);
-
+        //
     }
 
     /**
@@ -34,7 +27,7 @@ class StoreController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCityRequest $request)
     {
         //
     }
@@ -42,7 +35,7 @@ class StoreController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Store $store)
+    public function show(City $city)
     {
         //
     }
@@ -50,7 +43,7 @@ class StoreController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Store $store)
+    public function edit(City $city)
     {
         //
     }
@@ -58,7 +51,7 @@ class StoreController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Store $store)
+    public function update(UpdateCityRequest $request, City $city)
     {
         //
     }
@@ -66,7 +59,7 @@ class StoreController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Store $store)
+    public function destroy(City $city)
     {
         //
     }
