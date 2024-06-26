86d33bae310834b4fd0e1da3873bca2bac7678a4
mahjouba bouchouf <45915936+manomano500@users.noreply.github.com>
2024-06-26 23:04:42 +0200

    veremail
    Signed-off-by: mahjouba bouchouf <45915936+manomano500@users.noreply.github.com>

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class VerifyEmailController extends Controller
 {
@@ -15,6 +18,14 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
+        if (! hash_equals((string) $request->route('id'), (string) $request->user()->getKey())) {
+            abort(403, 'Invalid verification link.');
+        }
+
+        if (! hash_equals((string) $request->route('hash'), sha1($request->user()->getEmailForVerification()))) {
+            abort(403, 'Invalid verification link.');
+        }
+        Log::info('VerifyEmailController');
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(
                 'http://localhost:8080'.RouteServiceProvider::HOME.'?verified=1'
@@ -28,5 +39,24 @@ class VerifyEmailController extends Controller
         return redirect()->intended(
             'http://localhost:8080'.RouteServiceProvider::HOME.'?verified=1'
         );
+    }
+
+
+    public function generateVerificationUrl($userId): string
+    {
+        $user = User::find($userId);
+
+        if (!$user) {
+            abort(404, 'User not found');
+        }
+
+        $url = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            ['id' => $user->id, 'hash' => sha1($user->email)],
+            true // absolute URL
+        );
+
+        return $url;
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,6 +17,7 @@ class EventServiceProvider extends ServiceProvider
      * The event to listener mappings for the application.
      *
      * @var array<class-string, array<int, class-string>>
+     *
      */
     protected $listen = [
         Registered::class => [
