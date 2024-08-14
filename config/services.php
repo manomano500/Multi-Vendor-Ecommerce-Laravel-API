6b090aa2eb4ed8798823bbd6592e7d2827cae9de
mahjouba bouchouf <45915936+manomano500@users.noreply.github.com>
2024-08-14 14:10:51 +0200

    ,,
    Signed-off-by: mahjouba bouchouf <45915936+manomano500@users.noreply.github.com>

--- config/services.php
+++ config/services.php
@@ -35,12 +35,7 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
-    ],
-
-    'stripe' => [
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-    ],
+    ]
 
 
 
